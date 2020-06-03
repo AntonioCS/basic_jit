861157eafed9a8337f14edf5f626923a72822cff
AntonioCS <antoniocs@gmail.com>
2020-06-03 17:11:57 +0100

    Fixed opcodes

--- video1/src/main.cpp
+++ video1/src/main.cpp
@@ -9,9 +9,7 @@
 //little indian - order of bytes least significant to most significant
 namespace {
     constexpr auto asm_x64_rex_w = 0x48; //indicates instruction is x64
-    constexpr auto asm_rax = 0xC0;
-    constexpr auto asm_mov = 0xC7;
-    constexpr auto asm_res = 0xc3;
+    constexpr auto asm_ret = 0xc3;
 
     using i64 = int64_t;
     using u8 = uint8_t;
@@ -29,15 +27,16 @@ int main() {
     //c++ will not allow assignment to a void pointer (LPVOID)
     u8* memory_c = static_cast<u8*>(memory);
 
+    //mov    rax, 0x2a
     //{ 0x48, 0xC7, 0xC0, 0x2A, 0x00, 0x00, 0x00 }
     memory_c[0] = asm_x64_rex_w;
-    memory_c[1] = asm_mov;
-    memory_c[2] = asm_rax;
+    memory_c[1] = 0xC7;
+    memory_c[2] = 0xC0;
     memory_c[3] = 0x2A; //42
     memory_c[4] = 0x00;
     memory_c[5] = 0x00;
     memory_c[6] = 0x00;
-    memory_c[7] = asm_res;
+    memory_c[7] = asm_ret;
 
 
     i64 result = ((the_answer)memory)();
